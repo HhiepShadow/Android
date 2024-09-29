c397c062cec63dfba4f62891339d0c60be381df8
HHiepShadow <hth1292003@gmail.com>
2024-09-30 00:42:59 +0700

    Upload lesson 5

---
+++ app/src/main/java/com/example/lesson5_components/ContentProviders/MyContentProvider.java
@@ -0,0 +1,141 @@
+package com.example.lesson5_components.ContentProviders;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.HashMap;
+
+public class MyContentProvider extends ContentProvider {
+    private static final String AUTHORITY = "com.example.lesson5_components";
+    private static final String PATH_ITEMS = "items";
+    private static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + PATH_ITEMS);
+    private static final int ITEMS = 1;
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static HashMap<String, String> values;
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Set up for database:
+    private SQLiteDatabase db;
+    private static final String DATABASE_NAME = "ItemDB";
+    private static final String TABLE_NAME = "Items";
+    private static final int VERSION = 1;
+
+    private static final String CREATE_DB_TABLE = "CREATE TABLE " + TABLE_NAME +
+            " (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "name TEXT NOT NULL);";
+
+    static {
+        uriMatcher.addURI(AUTHORITY, PATH_ITEMS, ITEMS);
+        uriMatcher.addURI(AUTHORITY, PATH_ITEMS + "/*", ITEMS);
+    }
+
+    @Override
+    public boolean onCreate() {
+       Context context = getContext();
+       DatabaseHelper dbHelper = new DatabaseHelper(context);
+
+       db = dbHelper.getWritableDatabase();
+
+       return db != null;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        qb.setTables(TABLE_NAME);
+        switch (uriMatcher.match(uri)){
+            case ITEMS:
+                qb.setProjectionMap(values);
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+
+        if(s1 == null || s1 == ""){
+            s1 = ID;
+        }
+
+        Cursor cursor = qb.query(db, strings, s, strings1, null, null, s1);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        switch (uriMatcher.match(uri)){
+            case ITEMS:
+                return "vnd.android.cursor.dir/items";
+            default:
+                throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        long rowID = db.insert(TABLE_NAME, "", contentValues);
+
+        if(rowID > 0){
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+
+            getContext().getContentResolver().notifyChange(_uri, null);
+            return _uri;
+        }
+
+        throw new SQLiteException("Failed to add a record into " + uri);
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        int count = 0;
+        switch (uriMatcher.match(uri)){
+            case ITEMS:
+                count = db.update(TABLE_NAME, contentValues, s, strings);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return count;
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null,VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+            sqLiteDatabase.execSQL(CREATE_DB_TABLE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+            onCreate(sqLiteDatabase);
+        }
+    }
+}
--- app/src/main/java/com/example/lesson5_components/ContentProviders/README.md
+++ app/src/main/java/com/example/lesson5_components/ContentProviders/README.md
@@ -1,2 +1,52 @@
 ## CONTENT PROVIDERS
 
+- Content Provider is a component in Android allows data sharing between applications
+- A very important component that serves the purpose of a relational database to store the data of applications
+- Provides a standard interface for accessing and managing data
+- Allows applications to interact with each other securely and efficiently
+- Android system allows Content Provider to store data in several ways:
+  - Images
+  - Audio
+  - Videos
+  - Personal contact information ...
+by storing them in `SQLite`
+
+### Content URI:
+- A unique identifier used to access data within a Content Provider
+- Allows applications to interact with each other securely and efficiently through operations such as:
+  - query
+  - insert
+  - update
+  - delete
+- Structure of Content URI:
+``` 
+content://<authority>/<path>/<id>
+```
+in there:
+  - `content://`: Mandatory part of the URI as it represents that the given URI is Content URI
+  - `authority`: Content Provider name, defined in `AndroidManifest.xml`
+  - `path`: Path to specific data in the Content Provider
+  - `id`: ID of the specific record we want to access
+Ex: `content://com.example.provider/notes/1`
+
+### Operations in Content Provider:
+- Create
+- Read
+- Update
+- Delete
+
+### Working of the Content Provider:
+
+### Creating a Content Provider:
+- Step 1: Create a class extends `ContentProvider` base class
+- Step 2: To access the content, define a content provider URI address
+- Step 3: Create a database to store data
+- Step 4: Implement 6 abstract methods of ContentProvider class
+- Step 5: Register content provider in AndroidManifest.xml file using `<provider>` tag
+
+### 6 abstract methods:
+1. query(): 
+  - Accept 
+
+
+
